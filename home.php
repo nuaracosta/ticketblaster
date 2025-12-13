264bed0ebdbf16c15d405629db504248665f6179
josiedriscoll <77085967+josiedriscoll@users.noreply.github.com>
2025-12-12 21:25:35 -0500

    Update home.php

--- home.php
+++ home.php
@@ -31,7 +31,6 @@ $result = $stmt->get_result();
             <li><a href="home.php" class="active">Home</a></li>
             <li><a href="signin.php">Sign In</a></li>
             <li><a href="events.php">Upcoming Events</a></li>
-            <li><a href="signup.php">Sign Up</a></li>
         </ul>
     </nav>
 </header>
